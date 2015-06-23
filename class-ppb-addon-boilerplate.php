75c6f96d2c7d128d66f80d417a03d84f1df7cdac
Shramee <shramee.srivastav@gmail.com>
2015-06-24 00:16:01 +0530

    Initial Commit

---
+++ class-ppb-addon-boilerplate.php
@@ -0,0 +1,106 @@
+<?php
+
+class Pootle_Page_Builder_Addon_Boilerplate{
+
+	/**
+	 * Pootle_Page_Builder_Addon_Boilerplate Instance of main plugin class.
+	 *
+	 * @var 	object Pootle_Page_Builder_Addon_Boilerplate
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * The token.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public static $token;
+	/**
+	 * The version number.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public static $version;
+
+	/**
+	 * Pootle Page Builder Addon Boilerplate plugin directory URL.
+	 *
+	 * @var 	string Plugin directory
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	public static $url;
+
+	/**
+	 * Pootle Page Builder Addon Boilerplate plugin directory Path.
+	 *
+	 * @var 	string Plugin directory
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	public static $path;
+
+	/**
+	 * Main Pootle Page Builder Addon Boilerplate Instance
+	 *
+	 * Ensures only one instance of Storefront_Extension_Boilerplate is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @return Pootle_Page_Builder_Addon_Boilerplate instance
+	 */
+	public static function instance() {
+		if ( null == self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	} // End instance()
+
+	/**
+	 * Constructor function.
+	 *
+	 * @access  private
+	 * @since   1.0.0
+	 */
+	private function __construct() {
+
+		self::$token =     'ppb-addon-boilerplate';
+		self::$url =       plugin_dir_url( __FILE__ );
+		self::$path =      plugin_dir_path( __FILE__ );
+		self::$version =   '1.0.0';
+
+		add_action( 'init', array( $this, 'init' ) );
+	} // End __construct()
+
+	public function init() {
+
+		if ( defined( 'POOTLEPAGE_VERSION' ) && 0 < version_compare( POOTLEPAGE_VERSION, '2.5.0' ) ) {
+
+			$this->add_actions();
+			$this->add_filters();
+		}
+	} // End init()
+
+	private function add_actions() {
+
+		//Adding front end JS and CSS in /assets folder
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+	} // End add_actions()
+
+	private function add_filters() {
+
+		//Add Pootle Page Builder Filter hooks here
+	} // End add_filters()
+
+	public function enqueue() {
+		$token = self::$token;
+		$url = self::$url;
+
+		wp_enqueue_style( $token . '-css', $url . '/assets/front-end.css' );
+		wp_enqueue_script( $token . '-js', $url . '/assets/front-end.js', array( 'jquery' ) );
+	} // End enqueue()
+
+}
---
+++ pootle-pb-addon-boilerplate.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Pootle Page Builder Addon Boilerplate
+Plugin URI: http://pootlepress.com/
+Description: Boilerplate for fast track Pootle Page Builder Addon Development
+Author: Shramee
+Version: 0.1
+Author URI: http://shramee.com/
+*/
+
+/**
+ * Including Main Plugin class
+ */
+require_once 'class-ppb-addon-boilerplate.php';
+
+Pootle_Page_Builder_Addon_Boilerplate::instance();
