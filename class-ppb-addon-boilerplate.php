b4eb74e19b96b478d25676360a0acbe7e02d97a8
Shramee <shramee.srivastav@gmail.com>
2015-07-22 13:48:31 +0530

    Moving API init and documentation
    Correcting documentation, having initial version as 0.1.0 and moving
    PP API init to add on class to avoid updating version at three
    places...

--- class-ppb-addon-boilerplate.php
+++ class-ppb-addon-boilerplate.php
@@ -7,7 +7,7 @@ class Pootle_Page_Builder_Addon_Boilerplate{
 	 *
 	 * @var 	object Pootle_Page_Builder_Addon_Boilerplate
 	 * @access  private
-	 * @since 	1.0.0
+	 * @since 	0.1.0
 	 */
 	private static $_instance = null;
 
@@ -15,14 +15,14 @@ class Pootle_Page_Builder_Addon_Boilerplate{
 	 * The token.
 	 * @var     string
 	 * @access  public
-	 * @since   1.0.0
+	 * @since   0.1.0
 	 */
 	public static $token;
 	/**
 	 * The version number.
 	 * @var     string
 	 * @access  public
-	 * @since   1.0.0
+	 * @since   0.1.0
 	 */
 	public static $version;
 
@@ -31,7 +31,7 @@ class Pootle_Page_Builder_Addon_Boilerplate{
 	 *
 	 * @var 	string Plugin directory
 	 * @access  private
-	 * @since 	1.0.0
+	 * @since 	0.1.0
 	 */
 	public static $url;
 
@@ -40,7 +40,7 @@ class Pootle_Page_Builder_Addon_Boilerplate{
 	 *
 	 * @var 	string Plugin directory
 	 * @access  private
-	 * @since 	1.0.0
+	 * @since 	0.1.0
 	 */
 	public static $path;
 
@@ -49,7 +49,7 @@ class Pootle_Page_Builder_Addon_Boilerplate{
 	 *
 	 * Ensures only one instance of Storefront_Extension_Boilerplate is loaded or can be loaded.
 	 *
-	 * @since 1.0.0
+	 * @since 0.1.0
 	 * @return Pootle_Page_Builder_Addon_Boilerplate instance
 	 */
 	public static function instance() {
@@ -61,40 +61,54 @@ class Pootle_Page_Builder_Addon_Boilerplate{
 
 	/**
 	 * Constructor function.
-	 *
 	 * @access  private
-	 * @since   1.0.0
+	 * @since   0.1.0
 	 */
 	private function __construct() {
 
-		self::$token =     'ppb-addon-boilerplate';
-		self::$url =       plugin_dir_url( __FILE__ );
-		self::$path =      plugin_dir_path( __FILE__ );
-		self::$version =   '1.0.0';
+		self::$token   =   'ppb-addon-boilerplate';
+		self::$url     =   plugin_dir_url( __FILE__ );
+		self::$path    =   plugin_dir_path( __FILE__ );
+		self::$version =   '0.1.0';
 
 		add_action( 'init', array( $this, 'init' ) );
 	} // End __construct()
 
+	/**
+	 * Initiates the plugin
+	 * @action init
+	 * @since 0.1.0
+	 */
 	public function init() {
 
-		if ( class_exists( 'Pootle_Page_Builder' ) ) {
+		if ( defined( 'POOTLEPAGE_VERSION' ) && 0 < version_compare( POOTLEPAGE_VERSION, '2.5.0' ) ) {
 
-			$this->add_actions();
-			$this->add_filters();
+			//Add the required hooks
+			$this->add_hooks();
+
+			// Pootlepress API Manager
+			/** Including PootlePress_API_Manager class */
+			require_once( plugin_dir_path( __FILE__ ) . 'pp-api/class-pp-api-manager.php' );
+			/** Instantiating PootlePress_API_Manager */
+			new PootlePress_API_Manager( self::$token, 'Pootle Page Builder Addon Boilerplate', self::$version, __FILE__, self::$token );
 		}
 	} // End init()
 
-	private function add_actions() {
+	/**
+	 * Adds the hooks required
+	 * @since 0.1.0
+	 */
+	private function add_hooks() {
 
 		//Adding front end JS and CSS in /assets folder
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
-	} // End add_actions()
 
-	private function add_filters() {
-
-		//Add Pootle Page Builder Filter hooks here
 	} // End add_filters()
 
+	/**
+	 * Adds front end stylesheet and js
+	 * @since 0.1.0
+	 */
 	public function enqueue() {
 		$token = self::$token;
 		$url = self::$url;
